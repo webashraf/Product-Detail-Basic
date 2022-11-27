5338a7d7b1764f12c9511f84a7313e5e49b3d970
webashraf <aliashraftamim@gmail.com>
2022-11-27 22:38:34 +0600

    Practice react route and state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Components/Home/Home';
-import Shop from './Components/Shop/Shop';
-import NoFound from './Components/Note Found/NoFound';
-import PDetails from './Components/ProductDetails/PDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <BrowserRouter>
-            <Routes>
-              <Route exact path='/' element={ <Home/> }>
-              </Route>
-              <Route exact path='/home' element={ <Home/> }>
-              </Route>
-              <Route path='/shop/:pId' element={<PDetails />}>
-              </Route>
-              <Route path='*' element={<NoFound/>}>
-              </Route>
-            </Routes>
-          </BrowserRouter>
-      </header>
-    </div>
+
   );
 }
 
